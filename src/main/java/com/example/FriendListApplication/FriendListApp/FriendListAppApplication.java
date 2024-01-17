5c979cecdbc54ae021b5eeb745dcd8788f2d0005
TenshiRD <robbievandendoel@hotmail.com>
2024-01-17 21:46:45 +0100

    Built base application

---
+++ src/main/java/com/example/FriendListApplication/FriendListApp/FriendListAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.FriendListApplication.FriendListApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendListAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FriendListApplication/FriendListApp/FriendListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FriendListApplication.FriendListApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
